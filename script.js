074404e109e0356834ae0c0fd8c758b4ecba0908
Mahfuzul Haque <131514320+Haque-git@users.noreply.github.com>
2023-07-19 17:54:42 +0600

    updated

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function add(){
-  let todo = document.getElementById('text')
+function FizzBazz (n){
   
-
+  for (i = 1; i <= n; i++) {
+    const dict = []
+    if (i % 3 === 0) {
+      dict.push("Fizz")
+    }
+    if (i % 5 === 0) {
+      dict.push("Bazz")
+    }
+    console.log(dict.length === 0 ? i : dict.join(""))
+  }
 }
+FizzBazz(100)
